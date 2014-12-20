a3b5fabe10b7331a01fcc75c90932d23f752b9e3
urthbound <jeffowler@gmail.com>
2014-12-20 14:38:01 -0500

    first comment

--- 1-9.c
+++ 1-9.c
@@ -6,18 +6,14 @@ int main()
     char c;
     int spaces = 0;
     while ((c = getchar()) != EOF) {
-        if (c == ' ') {
-            /* if (spaces != 1) { ... } 
-               eliminate the redundant block that contains just ; 
+        if (c == ' ' && spaces != 1) {
+            /* if (spaces != 1) { ... }
+               eliminate the redundant block that contains just ;
                in fact pull this logic up so that line 9 reds
                if (c == ' ' && spaces != 1) {
                the style guidelines there is "reduce nesting in if statements if possible" */
-            if (spaces == 1) {
-                ;
-            } else {
-                spaces = 1;
-                printf(" ");
-            }
+            spaces = 1;
+            printf(" ");
         } else {
             spaces = 0;
             printf("%c", c);
